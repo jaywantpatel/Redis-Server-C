39de106b15bba53dee11fe6bac856e0eee3529a5
jaywantpatel <56215664+jaywantpatel@users.noreply.github.com>
2025-12-27 02:56:19 +0000

    Key Value operation setup and definations

--- include/RedisDatabase.h
+++ include/RedisDatabase.h
@@ -5,11 +5,25 @@
 #include <mutex>
 #include <unordered_map>
 #include <vector>
+#include <chrono>
 
 class RedisDatabase {
 public:
     //Get the singleton instance 
     static RedisDatabase& getInstance();
+
+    //Common Comands
+    bool flushAll();
+
+    //Key/Value operations 
+    void set(const std::string& key, const std::string& value);
+    bool get(const std::string& key, const std::string& value);
+    std::vector<std::string> keys();
+    std::string type(const std::string& key);
+    bool del(const std::string& key);
+    bool expire(const std::string& key, const std::string& seconds);
+    bool rename(const std::string& oldKey, const std::string& newKey);
+
 
     //Persistance: Dump / load the database from a file.
     bool dump(const std::string& filename);
@@ -27,6 +41,8 @@ private:
     std::unordered_map<std::string, std::string> kv_store;
     std::unordered_map<std::string, std::vector<std::string>> list_store;
     std::unordered_map<std::string, std::unordered_map<std::string, std::string>> hash_store;
+
+    std::unordered_map<std::string, std::chrono::steady_clock::time_point> expiry_map;
 
 };
 
--- src/RedisCommandHandler.cpp
+++ src/RedisCommandHandler.cpp
@@ -73,12 +73,69 @@ std::string RedisCommandHandler::processCommand(const std::string& commandLine) 
     RedisDatabase& db = RedisDatabase::getInstance();   
 
 //Check commands
+//Common Comands
     if (cmd == "PING") {
         response  << "+PONG\r\n";
     } else if (cmd == "ECHO") {
-        //....
-    }
+        if (tokens.size() < 2)
+            response << "-Error: ECHO requires a message\r\n";
+        else    
+            response << "+" << tokens[1] << "\r\n";
+    } else if (cmd == "FLUSHALL") {
+        db.flushAll();
+        response << "+OK\r\n";
+    } 
     // Key/Value operations 
+    else if (cmd == "SET") {
+        if (tokens.size() < 3)
+            response << "-Error: SET requires key and value\r\n";
+        else {
+            db.set(tokens[1], tokens[2]);
+            response << "+OK\r\n";
+        }
+    } else if (cmd == "GET") {
+        if (tokens.size() < 2)
+            response << "-Error: GET requires key and value\r\n";
+        else {
+            std::string value;
+            if (db.get(tokens[1], value))
+                response << "$" << value.size() << "\r\n" << value << "\r\n";
+            else 
+                response << "$-1\r\n";
+        }
+    } else if (cmd == "KEYS") {
+        std::vector<std::string> allKeys = db.keys();
+        response << "*" << allKeys.size() << "\r\n";
+        for (const auto& key : allKeys)
+            response << "$" << key.size() << "\r\n" << key << "\r\n";
+    } else if (cmd == "TYPE") {
+        if (tokens.size() < 2)
+            response << "-Error: TYPE requires key \r\n";
+        else
+            response << "+" << db.type(tokens[1]) << "\r\n";
+    } else if (cmd == "DEL" | cmd == "UNLINK") {
+        if (tokens.size() < 2)  
+            response << "-Error: " << cmd << "requires key\r\n";
+        else {
+            bool res = db.del(tokens[1]);
+            response << ":" << (res ? 1:0) << "\r\n";
+        }
+    } else if (cmd == "EXPIRE") {
+        if (tokens.size() < 3)
+            response << "-Error: EXPIRE requires key and time in seconds\r\n";
+        else {
+            if (db.expire(tokens[1], tokens[2]))
+                response << "+OK\r\n";
+        }
+            
+    } else if (cmd == "RENAME") {
+        if (tokens.size() < 3)
+            response << "-Error: RENAME requires old key name and new key name\r\n";
+        else {
+            if (db.rename(tokens[1], tokens[2]))
+                response << "+OK\r\n"; 
+        }   
+    }
     // List Operations 
     //Hash Operations 
     else {
